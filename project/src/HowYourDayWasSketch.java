be4908ad5fc46427918fa0533afef7a2337da74b
Chris Dzombak <chris@chrisdzombak.net>
2011-10-06 15:33:44 -0400

    add basic Processing sketch

---
+++ project/src/HowYourDayWasSketch.java
@@ -0,0 +1,20 @@
+import processing.core.PApplet;
+
+public class HowYourDayWasSketch extends PApplet {
+
+	private static final long serialVersionUID = -450426086211320391L;
+
+	public static void main(String args[]) {
+		PApplet.main(new String[] { "--present", "HowYourDayWasSketch" });
+	}
+
+	public void setup() {
+		// TODO
+		// for display, we'll likely steal a lot of the fonts and full-screen stuff from cdzombak's NoiseSketch (in processing-exercises)
+	}
+	
+	public void draw() {
+		// TODO
+	}
+	
+}
