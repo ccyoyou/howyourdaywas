598625425e72c0d6bf26324c6d80ddb317d2ae69
Torehan <torehan7@gmail.com>
2011-10-09 15:18:26 -0400

    pulling more of the feed

--- project/src/edu/umich/eecs498f11/howyourdaywas/FacebookSource.java
+++ project/src/edu/umich/eecs498f11/howyourdaywas/FacebookSource.java
@@ -9,11 +9,17 @@ import com.restfb.Parameter;
 import com.restfb.types.Post;
 
 public class FacebookSource implements DataSource {
-	FacebookClient fbClient = new DefaultFacebookClient("AAACEdEose0cBAN8DBtEtadKgnbx41jLFpiTMVb3RMfkBnZC7Ez4XICZATjXboO526zt9mEg60JFhhUbLQLJSkT4Pa96GLzs7fDKzHoAuKkNmF74eJC");
+	FacebookClient fbClient = new DefaultFacebookClient("AAACEdEose0cBAOribywQZBP7YLqBjqDD7uw1gqymDB1TSgF9sNSALbWHPGRmeWVVNag4cDCxe4b8kphOVfyWwCuwlBJHPua6GRlRgRZBYYP6vIE5dT");
 	public List<String> getData() {
-		Connection<Post> publicSearch = fbClient.fetchConnection("me/feed", Post.class);
+		Connection<Post> myFeed = fbClient.fetchConnection("me/statuses", Post.class);
 		final ArrayList<String> statuses = new ArrayList<String>();
-		statuses.add(publicSearch.getData().get(0).getMessage());
+		for (List<Post> myFeedConnectionPage : myFeed) {
+			for(Post post : myFeedConnectionPage) {
+				System.out.print(post.getCreatedTime());
+				System.out.print(post.getMessage());
+				statuses.add(post.toString());
+			}
+		}
 		
 		return statuses;
 	}
