0da92cc0a65deebfc68059fed198e1f8057a3bca
Chris Dzombak <chris@chrisdzombak.net>
2011-10-09 15:28:34 -0400

    add working FB code

--- project/src/edu/umich/eecs498f11/howyourdaywas/FacebookSource.java
+++ project/src/edu/umich/eecs498f11/howyourdaywas/FacebookSource.java
@@ -5,22 +5,22 @@ import java.util.List;
 import com.restfb.Connection;
 import com.restfb.DefaultFacebookClient;
 import com.restfb.FacebookClient;
-import com.restfb.Parameter;
 import com.restfb.types.Post;
 
 public class FacebookSource implements DataSource {
+	
 	FacebookClient fbClient = new DefaultFacebookClient("AAACEdEose0cBAOribywQZBP7YLqBjqDD7uw1gqymDB1TSgF9sNSALbWHPGRmeWVVNag4cDCxe4b8kphOVfyWwCuwlBJHPua6GRlRgRZBYYP6vIE5dT");
+	
 	public List<String> getData() {
-		Connection<Post> myFeed = fbClient.fetchConnection("me/statuses", Post.class);
+		final Connection<Post> feedConnection = fbClient.fetchConnection("me/statuses", Post.class);
 		final ArrayList<String> statuses = new ArrayList<String>();
-		for (List<Post> myFeedConnectionPage : myFeed) {
-			for(Post post : myFeedConnectionPage) {
-				System.out.print(post.getCreatedTime());
-				System.out.print(post.getMessage());
-				statuses.add(post.toString());
-			}
+
+		final List<Post> posts = feedConnection.getData();
+		for(final Post post : posts) {
+			System.out.println(post.getUpdatedTime() + ": " + post.getMessage());
+			statuses.add(post.getMessage());
 		}
-		
+
 		return statuses;
 	}
 }
